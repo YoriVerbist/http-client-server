dc2ef38fc68ac5de87ac22a794b75911bdf4f962
Yori Verbist <yori.verbist1212@gmail.com>
2022-04-18 11:58:43 +0200

    Changes

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import argparse
+import re
 from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser(description="A simple HTTP client that supports GET, HEAD and PUT requests")
@@ -19,28 +20,43 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
+FORMAT = "utf-8"
 
 # create an INET, STREAMing socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(10)
 
 def find_html_code(response):
     return response.decode().split("\r\n\r\n")[1]
 
+def get_request_length(uri:str) -> int:
+    request = f"HEAD / HTTP/1.1\r\nHost:{args.uri}\r\nAccept:text/html\r\n\r\n"
+    s.sendall(request.encode())
+    response = s.recv(1024).decode(FORMAT)
+    # regex searches for the Content-Length variable in the response and gets the corresponding value
+    return int(re.findall("Content-Length:\s+\d+\s+", response)[0].strip("\r\n\r\n").strip("Content-Length: "))
+
 def main():
     s.connect((args.uri, args.port))
+    filename = args.uri[args.uri.find("/") + 1:]
+    print(filename)
+
     match args.command:
         case "GET":
-            request = f"{args.command} / HTTP/1.1\r\nHost:{args.uri}\r\nConnection: close\r\n\r\n"
+            request_length = get_request_length(args.uri)
+            print(type(request_length))
+            input()
+            request = f"{args.command} / HTTP/1.1\r\nHost:{args.uri}\r\nAccept:text/html\r\n\r\n"
             s.sendall(request.encode())
+
             response = b""
-            try:
-                while True:
-                    response += s.recv(4096);
-            except:
-                print("Session timed out!")
-            print(response.decode())
-            #print(response.decode().split('\r\n\r\n')[1].split('\n')[1])
+            while True:
+                data = s.recv(4096)
+                print(data)
+                if len(data) <= 0: 
+                    break
+                response += data
+                print(response)
+            print(data.decode("utf-8"))
             # Split of the headers from the html file
             html = find_html_code(response)
             with open ("index.html", "w") as file:
